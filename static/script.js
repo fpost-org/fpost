385e3336af3c051abb5763ee82457f877a242f22
Amir Farukshin <farukshin.amir@gmail.com>
2017-05-08 11:46:12 +0200

    core(js): #14 backlight incorrect filling of fields

--- static/script.js
+++ static/script.js
@@ -1,6 +1,13 @@
 addEventListener("keydown", keydown);
 autosize(document.querySelector('textarea'));
 
+$(".field").change(function() {
+	if($(this).hasClass("incorrect")){
+		$(this).removeClass("incorrect");
+		show_error(false);	
+	}
+});
+
 function getXmlHttp() {
     var xmlhttp;
     try {
@@ -18,7 +25,17 @@ function getXmlHttp() {
     return xmlhttp;
 }
 
+function send_push(){
+	
+	if(fields_incorrect() === false){
+		//alert('incorrect');
+	}else{
+		result();
+	}
+}
+
 function result() {
+	
     var a = "title=" + document.getElementById("title").value + "&name=" + document.getElementById("name").value + "&post=" + document.getElementById("post").value;
     var xmlhttp = getXmlHttp();
     xmlhttp.open('POST', '', true);
@@ -46,4 +63,36 @@ function nextfocus(){
 	if($("#title").is(":focus") ){
 		document.getElementById('name').focus();			
 	}
+}
+
+function fields_incorrect(){
+	var title = $("#title").val();
+	var name = $("#name").val();
+	var post = $("#post").val();
+	var key = true;
+	
+	if ($("#title").val().length <= 2){
+		$("#title").addClass("incorrect");
+		show_error(true, "field is too short");
+		key = false;	
+	}
+	if ($("#name").val().length <= 1){
+		$("#name").addClass("incorrect");
+		show_error(true, "field is too short");
+		key = false;	
+	}
+	return key;
+}
+
+function show_error(status = true, value = "", add = false){
+	if(status === true){
+		if(add === true){
+			var t = $("#error").text();
+			$("#error").text(t + "<br>" + value);
+		}else{
+			$("#error").text(value);
+		}		
+	}else if(status === false){
+		$("#error").text('');
+	}	
 }
