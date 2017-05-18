07633086052c563620c92fe7e30d2f4a03a4621c
Amir Farukshin <farukshin.amir@gmail.com>
2017-05-18 20:12:07 +0200

    cc

--- setup.sh
+++ setup.sh
@@ -1,18 +1,13 @@
-apt-get install postgresql-9.2
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-9.5 libpq-dev
 
-
-/etc/postgresql/9.2/main
-postgresql.conf:
-data_directory = '/var/lib/postgresql/9.2/main' 
-listen_addresses = '*'
-
-pg_hba.conf:
-host    all             all             0.0.0.0/0               md5
-
-su postgres
-/usr/bin/psql -d template1
-\c postgres
-\password postgres
-
-
-/etc/init.d/postgresql restart
+# edit /etc/postgresql/9.3/main/pg_hba.conf manualy
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
+# alter password 
+sudo passwd postgres 
+su postgres 
+psql -c "ALTER USER postgres WITH PASSWORD 'postgres'" -d template1 
+exit
+sudo /etc/init.d/postgresql restart
