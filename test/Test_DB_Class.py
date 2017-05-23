bc54217b97fd482ba4338300313e095b0552edc6
Amir Farukshin <farukshin.amir@gmail.com>
2017-05-23 20:34:25 +0200

    core: ci test check

--- test/Test_DB_Class.py
+++ test/Test_DB_Class.py
@@ -11,12 +11,17 @@ class TestDB(unittest.TestCase):
         name = hmac.new(bytearray('signature','utf-8'), bytearray(str(random.random()),'utf-8'), hashlib.sha256).hexdigest()
         post = hmac.new(bytearray('signature','utf-8'), bytearray(str(random.random()),'utf-8'), hashlib.sha256).hexdigest()
         DB.create_db();
-        DB.insert_db(title, name, post)
+        DB.insert(title, name, post)
         res = DB.getpostbyname(title)
         flag = False
         if (res[0]==title and res[1]==name and res[2]==post):
             flag = True
         self.assertTrue(flag)
-
+    
+    
+    def check(self):
+        flag = DB.check()
+        self.assertTrue(flag)
+     
 if __name__ == '__main__':
     unittest.main()
